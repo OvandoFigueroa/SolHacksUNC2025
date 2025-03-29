83164bc2bdea9d9e2dad33e43712f55837fb0541
Obeth Ovando <0b3thovando@gmail.com>
2025-03-29 11:20:47 -0400

    OrganizationsPageCreation
    I created the organization page with a simple text on it so something is there

---
+++ src/OrganizationsPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+const OrganizationsPage = () => {
+    return (
+        <div>
+            <h1>
+                <p>
+                    Organizations list:
+                </p>
+            </h1>
+        </div>
+
+
+    );
+};
+
+export default OrganizationsPage;
