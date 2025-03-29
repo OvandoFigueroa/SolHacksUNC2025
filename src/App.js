d0577a8e034f84cc91f4ddfe2f32904b273aaf91
Amir Dillard <dillard24a@ncssm.edu>
2025-03-29 10:47:32 -0400

    push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./HomePage";
+import Organizations from "./OrganizationsPage";
+import Login from "./LoginPage";
+import Messageboard from "./MessageboardPage";
+import Privatechat from "./PrivatechatPage";
+import Chatbot from "./ChatbotPage";
 
-function App() { 
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router> 
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Organizations" element={<OrganizationsPage />} />
+        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/Messageboard" element={<MessageboardPage />} />
+        <Route path="/Privatechat" element={<PrivatechatPage />} />
+        <Route path="/Chatbot" element={<ChatbotPage />} />
+      </Routes>
+    </Router>
   );
 }
 
